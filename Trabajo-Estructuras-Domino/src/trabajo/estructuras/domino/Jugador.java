844d9c1cf7b6b9e07d035677025db855bc24c747
iisraabr <israelba2007@gmail.com>
2026-05-06 16:41:33 +0200

    1

--- Trabajo-Estructuras-Domino/src/trabajo/estructuras/domino/Jugador.java
+++ Trabajo-Estructuras-Domino/src/trabajo/estructuras/domino/Jugador.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package trabajo.estructuras.domino;
 
-/**
- *
- * @author Usuario
- */
-public class Jugador {
-    
+import java.util.ArrayList;
+
+public class Jugador implements JugadorITF {
+
+    private ArrayList<Ficha> mano;
+    private String nombre;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.mano = new ArrayList<Ficha>();
+    }
+
+    @Override
+    public void robarFicha(Monton monton) {
+        if (!monton.estaVacio()) {
+            Ficha f = monton.extraerFicha();
+            mano.add(f);
+        }
+    }
+
+    @Override
+    public void mostrarMano() {
+        System.out.println("Fichas " + nombre + ":");
+
+        for (int i = 0; i < mano.size(); i++) {
+            System.out.print(mano.get(i).toString() + " ");
+        }
+        System.out.println();
+        System.out.println("Numero de ficha:");
+        for (int i = 0; i < mano.size(); i++) {
+            System.out.print("  " + i + "   ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public Ficha elegirFicha(int posicion) {
+
+        if (posicion >= 0 && posicion < mano.size()) {
+            return mano.remove(posicion);
+        }
+        return null;
+    }
+
+    @Override
+    public int numFichas() {
+        return mano.size();
+    }
+
+    @Override
+    public int sumarPuntosMano() {
+        int total = 0;
+        for (int i = 0; i < mano.size(); i++) {
+            total += mano.get(i).getsuma();
+        }
+        return total;
+    }
 }
